e3e3f230cd61cbd794dc4c141dcd49881b77bf41
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-05-15 08:31:10 +0500

    Improvments
    Enable Child Reaping
    Pick the Status returned by child through signals (SIGCHLD) and print it on screen
    Ensure that you do not miss any SIGCHLDÃ‚Â so use SA_NODEFER
    Create another version where you enable SIGCHLD through sigprocmask instead of SA_NODEFER
    Explain why Version in point #3 above is more safe than version in point #4 (Hint: Point # 4 may lose some statuses)

---
+++ ExternalErrorHandling/S1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <signal.h>
+ 
+
+void sighandler(int sig, siginfo_t *info, void *ucontext){
+	char buff[100];
+	pid_t mypid = info->si_pid;;
+    int signo=info->si_signo;
+    int status=info->si_status;
+     write(STDOUT_FILENO, "SIG\n", sizeof("SIG\n"));
+	sleep(1);
+	int count = sprintf(buff, "\nPID:%d \nSIGNO:%d   \nSTATUS:%d", mypid,signo,status);
+    write(STDOUT_FILENO, buff, count);
+    sleep(1);
+}
+
+
+int main() {
+    
+ printf("PID: %d\n", getpid());
+
+ 	//using sigaction stuct
+ 	struct sigaction action;
+
+ 	action.sa_sigaction=&sighandler;
+ 	action.sa_flags= SA_SIGINFO|SA_NOCLDWAIT|SA_NODEFER;
+ 	sigaction(SIGCHLD,&action,NULL);
+ 	 
+ 	for (int i = 0; i < 4; ++i)
+ 	{
+ 		int pid=fork();
+ 		if(pid==0){
+ 			execlp("gnome-calculator","gnome-calculator",NULL);
+ 		}
+ 	}
+ 	
+
+    pause();
+    for(;;){
+
+    }
+
+
+   
+
+
+    return 0;
+}
+
+
